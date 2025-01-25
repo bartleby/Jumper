afdfce1af1c1db5812e6551847b05a75ed7a5c49
Aleksandr Sabry <sabry.smk@gmail.com>
2025-01-25 16:15:04 +0800

    xxx

--- Sources/Jumper/SwiftUISupport.swift
+++ Sources/Jumper/SwiftUISupport.swift
@@ -11,25 +11,65 @@ public protocol SwiftUIScreenPresentable {
 public struct SwiftUIScreen<Content: View>: ScreenViewPresentable {
     private let content: Content
     private let modalPresentationStyle: UIModalPresentationStyle
+    private let presenter: UIKitPresenter?
     
     public init(
         content: Content,
-        modalPresentationStyle: UIModalPresentationStyle = .automatic
+        modalPresentationStyle: UIModalPresentationStyle = .automatic,
+        presenter: UIKitPresenter? = nil
     ) {
         self.content = content
         self.modalPresentationStyle = modalPresentationStyle
+        self.presenter = presenter
     }
     
     public func view() -> ScreenView {
-        let hostingController = UIHostingController(rootView: content)
+        let hostingController = UIHostingController(rootView: 
+            content.environment(\.uiKitPresenter, presenter)
+        )
         hostingController.modalPresentationStyle = modalPresentationStyle
         return hostingController
     }
 }
 
+// Модификаторы для SwiftUI
 extension View {
     public func modalPresentationStyle(_ style: UIModalPresentationStyle) -> some View {
         environment(\.modalPresentationStyle, style)
+    }
+    
+    public func presentUIKit<Content: View>(
+        style: UIModalPresentationStyle = .automatic,
+        isPresented: Binding<Bool>,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View {
+        self.background(
+            UIKitPresenterView(
+                isPresented: isPresented,
+                style: style,
+                content: content
+            )
+        )
+    }
+}
+
+private struct UIKitPresenterView<Content: View>: UIViewControllerRepresentable {
+    @Binding var isPresented: Bool
+    let style: UIModalPresentationStyle
+    let content: () -> Content
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        UIViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        if isPresented {
+            let hostingController = UIHostingController(rootView: content())
+            hostingController.modalPresentationStyle = style
+            uiViewController.present(hostingController, animated: true)
+        } else {
+            uiViewController.dismiss(animated: true)
+        }
     }
 }
 
